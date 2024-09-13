667dec33aa5483fbc32fd875de52768b9e87d1ec
MohammedNasser12 <mohamednasser827@yahoo.com>
2024-09-14 00:14:49 +0300

    Create watch_list_view.dart

---
+++ lib/tabs/watchlist_tab/view/watch_list_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/search/search_item_widget.dart';
+
+class WatchListView extends StatelessWidget {
+  const WatchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Watch List",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+              // textAlign: TextAlign.start,
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemBuilder: (context, index) => SearchItemWidget(),
+              itemCount: 13,
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
