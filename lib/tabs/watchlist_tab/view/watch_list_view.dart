3799b971704214330707ad31319c619f7a9460ed
MohammedNasser12 <mohamednasser827@yahoo.com>
2024-09-16 22:21:39 +0300

    Update watch list UI

--- lib/tabs/watchlist_tab/view/watch_list_view.dart
+++ lib/tabs/watchlist_tab/view/watch_list_view.dart
@@ -12,13 +12,16 @@ class WatchListView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              "Watch List",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "Watch List",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+                // textAlign: TextAlign.start,
               ),
-              // textAlign: TextAlign.start,
             ),
             Expanded(
                 child: ListView.builder(
