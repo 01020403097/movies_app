1f5ff4f15fef51ce9e2b0c6f47da4e4f728148ad
MohammedNasser12 <mohamednasser827@yahoo.com>
2024-09-16 22:21:53 +0300

    Update search UI

--- lib/tabs/search_tab/view/search_item_widget.dart
+++ lib/tabs/search_tab/view/search_item_widget.dart
@@ -7,76 +7,69 @@ class SearchItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Expanded(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, top: 30),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Stack(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10, top: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  "assets/images/watchlistimage.png"),
+                              fit: BoxFit.fill)),
+                      height: 89,
+                      width: 140,
+                      child: Container(
+                        child: Image.asset(
+                          "assets/images/watchlisttext.png",
+                          scale: 3,
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/images/bookmark.png",
+                      scale: 4,
+                      alignment: Alignment.topRight,
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            // color: Colors.amber,
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    "assets/images/watchlistimage.png"),
-                                fit: BoxFit.fill)),
-                        height: 100,
-                        width: 150,
-                        child: Container(
-                          child: Image.asset(
-                            "assets/images/watchlisttext.png",
-                            scale: 3,
-                          ),
-                          alignment: Alignment.center,
+                      Text(
+                        "Alita Battle Angel",
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
-                      Image.asset(
-                        "assets/images/bookmark.png",
-                        scale: 4,
-                        alignment: Alignment.topRight,
-                      )
+                      Text(
+                        "2019",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Rosa Salazar, Christoph Waltz",
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Alita Battle Angel",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "2019",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          "Rosa Salazar, Christoph Waltz",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        // Divider(
-                        //   thickness: 2,
-                        //   color: Colors.white,
-                        // ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Divider(
-                thickness: 2,
-                color: Color(0xFF707070),
-              )
-            ],
-          ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(
+              thickness: 2,
+              color: Color(0xFF707070),
+            )
+          ],
         ),
       ),
     );
